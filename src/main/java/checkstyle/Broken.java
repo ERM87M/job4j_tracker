ae8b81c67dac235d0004eb1538e3f33afdb9e20a
ERM87M <maziratira@gmail.com>
2025-01-02 22:52:27 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/checkstyle/Broken.java
@@ -0,0 +1,31 @@
+package checkstyle;
+
+public class Broken {
+    private final int sizeOfEmpty = 10;
+
+    public String surname;
+    public static final String NEW_VALUE = "";
+
+    private String name;
+
+    public Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d) {
+
+    }
+}
+
+
+
+
+
