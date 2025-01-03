038f79efa80e06df944514f3287d17a175829b4d
ERM87M <maziratira@gmail.com>
2025-01-04 02:01:24 +0300

    1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+import java.util.concurrent.Callable;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println("The cat " + this.name + " was eating " + this.food);
+    }
+
+    public static void main(String[] args) {
+        Cat barsik = new Cat();
+        barsik.giveNick("Barsik");
+        barsik.eat("fish");
+        barsik.show();
+
+    }
+}
+
