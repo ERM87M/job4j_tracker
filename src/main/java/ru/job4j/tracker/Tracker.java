ad9a34338890a53942a180ea7e018e64a86eea3d
ERM87M <maziratira@gmail.com>
2025-01-18 19:33:21 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,6 +13,17 @@ public class Tracker {
         return item;
     }
 
+    private int indexOf(int id) {
+        int result = -1;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
 
@@ -31,14 +42,17 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            item.setId(id);
+            items[index] = item;
         }
-        return rsl;
+        return result;
     }
 }
