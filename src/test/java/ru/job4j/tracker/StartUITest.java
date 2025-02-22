49dd877e41a324ce26dc469b8d7a6c41aa88f7ce
ERM87M <maziratira@gmail.com>
2025-02-22 19:18:34 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -187,4 +187,25 @@ class StartUITest {
         );
     }
 
+    @Test
+    void whenInvalidExit() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Input input = new MockInput(
+                new String[]{"7", "0"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln
+                        + "0. Завершить программу" + ln
+                        + "Неверный ввод, вы можете выбрать: 0 .. 0" + ln
+                        + "Меню:" + ln
+                        + "0. Завершить программу" + ln
+                        + "=== Завершение программы ===" + ln
+        );
+    }
 }
