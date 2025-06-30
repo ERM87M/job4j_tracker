5c5081d724d756e2994bee0d122e6fb82161bb8a
ERM87M <maziratira@gmail.com>
2025-06-30 20:30:02 +0300

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenNotFound() {
+        PhoneDictionary phone = new PhoneDictionary();
+        phone.add(new Person("Денис", "Лимитц", "3421123", "Москва"));
+        ArrayList<Person> result = phone.find("Калининград");
+        assertThat(phone.find("Roman")).isEmpty();
+    }
+}
