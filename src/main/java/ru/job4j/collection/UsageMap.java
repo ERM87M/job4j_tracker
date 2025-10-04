6bc5b6940cd96386bb24f101fb74ea7ef9a2579e
ERM87M <maziratira@gmail.com>
2025-10-05 01:32:23 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("Max@yandex.ru", "Max Max");
+        map.put("Sam@yandex.ru", "Sam Samuel");
+        map.put("Yoto@yandex.ru", "Yoto Toto");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
