d8f4c991bda839a509b0455b7b74255c33f92ac3
ERM87M <maziratira@gmail.com>
2025-10-05 02:07:44 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenAddDuplicatePassportThenFalse() {
+        Citizen citizenOne = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizenTwo = new Citizen("2f44a", "Artem Artemov");
+        PassportOffice office = new PassportOffice();
+        boolean firstAdd = office.add(citizenOne);
+        assertThat(firstAdd).isTrue();
+        boolean secondAdd = office.add(citizenTwo);
+        assertThat(secondAdd).isFalse();
+    }
+}
