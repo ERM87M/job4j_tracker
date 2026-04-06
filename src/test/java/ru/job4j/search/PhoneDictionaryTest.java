fdc66689a8f3e9d72b950b3a3166eab4b6d4a840
ERM87M <maziratira@gmail.com>
2026-04-06 16:38:01 +0300

    7. Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -10,7 +10,9 @@ public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
         PhoneDictionary phones = new PhoneDictionary();
-        phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
